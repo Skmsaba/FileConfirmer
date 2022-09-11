eb4bd0cb49ec5782c0743b3578d51beeac196bb9
Skmsaba <111267907+Skmsaba@users.noreply.github.com>
2022-09-11 19:40:11 +0530

    Variable assigned in Config file
    Updated on 11092022 19:40

--- src/main/java/com/learnJava/mail/FileTester.java
+++ src/main/java/com/learnJava/mail/FileTester.java
@@ -3,9 +3,10 @@ package com.learnJava.mail;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,18 +16,31 @@ public class FileTester {
 	public String fileConfirmer() throws IOException {
 		// TODO Auto-generated method stub
 		
+		SimpleDateFormat sdf=new SimpleDateFormat();
+		
+		Date date=new Date();
+		
+		String currentDate=sdf.format(date);
+		
         Properties prop=new Properties();
 		
 		FileInputStream fis=new FileInputStream("C:\\Users\\Admin\\eclipse-workspace\\mail\\src\\main\\java\\config.properties");
 		
 		prop.load(fis);
+		
+		String directory = prop.getProperty("directory");
+		String filePath = prop.getProperty("filePath");
+		String regex = prop.getProperty("regex");
+		String regex2 = prop.getProperty("regex2");
+		final String fileNameOrig = prop.getProperty("fileNameOrig");
+
 
 		String fileName,output = null;
 		
 		int lineCount = 0,k=0;
 
 		try {
-			File path = new File("C:\\Users\\Admin\\eclipse-workspace\\mail");
+			File path = new File(directory);
 
 			File[] files = path.listFiles();
 
@@ -34,57 +48,71 @@ public class FileTester {
 
 				fileName = file.getName();
 
-				String regex = "^RWA_MARKETRISK_\\d{4}(0?[1-9]|1[012])(0?[1-9]|[12][0-9]|3[01]).txt$";
 				Pattern pattern = Pattern.compile(regex);
 				Matcher match = pattern.matcher(fileName);
 
 				if (match.find()) {
-
-					BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\Admin\\eclipse-workspace\\mail\\"+fileName));
-
-					String line = null;
-
-					while ((line = br.readLine()) != null) {
-
-						lineCount++;
+					
+					System.out.println(fileName);
+					
+					System.out.println(fileNameOrig);
+					
+					System.out.println(fileNameOrig == fileName);
+					
+					if(fileName.matches(fileNameOrig)) {
 						
-						line=line.replaceAll("[^\\d]", " ");
-						line=line.trim();
-						line=line.replaceAll(" + ", " ");
-										
-						String regex2="\\d";
-						Pattern pattern2=Pattern.compile(regex2);
-						Matcher match2=pattern2.matcher(line);
+						System.out.println("Filename and FileOrig Match");
 						
-						if(match2.find()) {
+						BufferedReader br = new BufferedReader(new FileReader(filePath));
+
+						String line = null;
+
+						while ((line = br.readLine()) != null) {
+
+							lineCount++;
 							
-							k=Integer.parseInt(line);
+							line=line.replaceAll("[^\\d]", " ");
+							line=line.trim();
+							line=line.replaceAll(" + ", " ");
+											
+							Pattern pattern2=Pattern.compile(regex2);
+							Matcher match2=pattern2.matcher(line);
 							
-							System.out.println(k);
+							if(match2.find()) {
+								
+								k=Integer.parseInt(line);
+								
+								System.out.println(k);
+							}
+
 						}
+						
+						System.out.println("lineCount is"+ lineCount);
+						
+						if(k==lineCount) {
+							
+							output = "Linecount Verified in given file\nCount found OK";
+							
+						}else {
+							
+							output = "Linecount Verified in given file\nCount found Not OK";
+							
+						}
+						
+						System.out.println(output);
+						
+						br.close();
+						
+					}
 
-					}
-					
-					System.out.println("lineCount is"+ lineCount);
-					
-					if(k==lineCount) {
-						
-						output = "Linecount Verified in given file\nCount found OK";
-						
-					}else {
-						
-						output = "Linecount Verified in given file\nCount found Not OK";
-						
-					}
-					
 					System.out.println("Completed");
-					
-					br.close();
 					
 					return output;
 				
 				}
+								
 			}
+			
 		} catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
